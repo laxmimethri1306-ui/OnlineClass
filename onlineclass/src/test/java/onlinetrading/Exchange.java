9a1d2bd9a9ca567952138d6e1f38e8207a4bd24c
laxmimethri1306-ui <laxmimethri1306@gmail.com>
2026-05-19 10:26:49 +0530

    yugit

---
+++ onlineclass/src/test/java/onlinetrading/Exchange.java
@@ -0,0 +1,13 @@
+package onlinetrading;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Exchange {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+	}
+
+}
